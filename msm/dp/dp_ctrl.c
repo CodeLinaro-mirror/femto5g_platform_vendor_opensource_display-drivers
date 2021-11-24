a10799fbdfe6f1f2e876f3ff6534ab0245c1ddea
Manting Yao <mantyao@qti.qualcomm.com>
2021-11-23 19:52:23 -0500

    disp: msm: dp: fail the stream on request when link clock is off
    Fail the stream on request, when link clock is off. Otherwise, accessing
    register in link clock domain will stall the CPU.
    
    Change-Id: If3e431fccdc7746f219cc40b1ed49ecdd527b4ab
    Signed-off-by: Jim Wang <quic_jimwan@quicinc.com>
    Signed-off-by: Manting Yao <mantyao@qti.qualcomm.com>

--- msm/dp/dp_ctrl.c
+++ msm/dp/dp_ctrl.c
@@ -1278,6 +1278,10 @@ static int dp_ctrl_stream_on(struct dp_ctrl *dp_ctrl, struct dp_panel *panel)
 
 	ctrl = container_of(dp_ctrl, struct dp_ctrl_private, dp_ctrl);
 
+	/* Abort is requested, exit */
+	if (atomic_read(&ctrl->aborted))
+		return -EPERM;
+
 	if (!ctrl->power_on) {
 		DP_DEBUG("controller powered off\n");
 		return -EPERM;
