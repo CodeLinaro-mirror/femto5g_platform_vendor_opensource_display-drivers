2356f67fb683ec03a46baab714d626c2d3d24ef2
Ying Nie <quic_yingnie@quicinc.com>
2022-02-27 17:58:43 +0800

    remove duplicate license
    Change-Id: Ia43efa4665db854e2434dab11c33162a4020fd4d

--- msm/dsi/dsi_clk_manager.c
+++ msm/dsi/dsi_clk_manager.c
@@ -2,7 +2,6 @@
 /*
  * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
  * Copyright (c) 2016-2020, The Linux Foundation. All rights reserved.
- * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #include <linux/of.h>
--- msm/dsi/dsi_display.h
+++ msm/dsi/dsi_display.h
@@ -2,7 +2,6 @@
 /*
  * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
  * Copyright (c) 2015-2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #ifndef _DSI_DISPLAY_H_
