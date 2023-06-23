CodeLinaro-mirror/le_platform_vendor_opensource_display-drivers

disp: msm: dp: restart the DP session with normal mode for suspend to sim mode
If the DP driver suspend at simulation mode while the HPD is high, and resume
with HPD stay high as well, we end up with simulation mode. But assuming the
sink device has been restarted as well during the STR, so we need to restart
a new hotplug session with normal mode in this case.

Change-Id: Idb368e5bc6cc9335e74ed7a2118f7b332d49045b
Signed-off-by: Jim Wang <quic_jimwan@quicinc.com>

msm/dp/dp_mst_sim.h
@@ -1,6 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /*
  * Copyright (c) 2019-2020, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #ifndef _DP_MST_SIM_H_
@@ -24,6 +25,8 @@ int dp_sim_create_bridge(struct device *dev,
 		struct msm_dp_aux_bridge **bridge);
 
 int dp_sim_destroy_bridge(struct msm_dp_aux_bridge *bridge);
+
+u32 dp_sim_get_sim_mode(struct msm_dp_aux_bridge *bridge);
 
 int dp_sim_set_sim_mode(struct msm_dp_aux_bridge *bridge, u32 sim_mode);
 

CodeLinaro-mirror/femto5g_platform_vendor_opensource_display-drivers

msm/dp/dp_mst_sim.h
@@ -1,4 +1,6 @@
-/* Copyright (c) 2019-2021, The Linux Foundation. All rights reserved.
+/*
+ * Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2019-2021, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -31,6 +33,8 @@ int dp_sim_create_bridge(struct device *dev,
 		struct dp_aux_bridge **bridge);
 
 int dp_sim_destroy_bridge(struct dp_aux_bridge *bridge);
+
+u32 dp_sim_get_sim_mode(struct dp_aux_bridge *bridge);
 
 int dp_sim_set_sim_mode(struct dp_aux_bridge *bridge, u32 sim_mode);
 