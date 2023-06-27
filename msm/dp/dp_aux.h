b6511bcd7f1810b9d0c9582cf30cb6ff422a577e
Nilesh Laad <quic_nlaad@quicinc.com>
2023-06-27 13:01:37 +0530

    disp: msm: dp: add drm_dp_cec in display-drivers
    Port drm_dp_cec from common kernel to display-drivers.
    Fix compilation issues.
    
    Change-Id: Ib68f69339bc7692ab38aefd39297f83c0044d72d
    Signed-off-by: Nilesh Laad <quic_nlaad@quicinc.com>

--- msm/dp/dp_aux.h
+++ msm/dp/dp_aux.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /*
- * Copyright (c) 2022, Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2022-2023, Qualcomm Innovation Center, Inc. All rights reserved.
  * Copyright (c) 2012-2021, The Linux Foundation. All rights reserved.
  */
 
@@ -9,6 +9,7 @@
 
 #include "dp_catalog.h"
 #include "drm/drm_dp_helper.h"
+#include "drm_dp_cec.h"
 #include "dp_aux_bridge.h"
 
 #define DP_STATE_NOTIFICATION_SENT          BIT(0)
