12c5eea6254478c2bc6ff6d3a6526468b5cd9ccb
Ashish Kumar <quic_ashik@quicinc.com>
2024-02-29 09:05:08 +0530

    disp: msm: dp: add DRM_DP_CEC Flag for enabling CEC
    Add flag to enable CEC header inclusion for DP module.
    
    Change-Id: I5d213f42ecdaf40777d866e142e1391d116de9b7
    Signed-off-by: Ashish Kumar <quic_ashik@quicinc.com>

--- config/gki_kalamadispconf.h
+++ config/gki_kalamadispconf.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /*
- * Copyright (c) 2021-2022 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2021-2022, 2024 Qualcomm Innovation Center, Inc. All rights reserved.
  * Copyright (c) 2021, The Linux Foundation. All rights reserved.
  */
 
@@ -22,3 +22,4 @@
 #define CONFIG_HDCP_QSEECOM 1
 #define CONFIG_DRM_SDE_VM 1
 #define CONFIG_QTI_HW_FENCE 1
+#define CONFIG_DRM_DP_CEC 1
