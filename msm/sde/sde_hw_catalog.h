c623aa4bee9ecc0a07e1a388be896206e4d99b7b
Balaji Vekatesh Kalkonda <quic_bkalkond@quicinc.com>
2024-06-26 11:01:23 +0530

    disp: msm: sde: Enable hw catalog changes for Ravelin
    Enable hw catalog changes for Ravelin.
    
    Change-Id: I88a040551f1bd716a846da65327a5c660531de3b
    Signed-off-by: Balaji Vekatesh Kalkonda <quic_bkalkond@quicinc.com>
    (cherry picked from commit 3c642c7cb9a1e768bce7e0f8f896d38d2c250106)

--- msm/sde/sde_hw_catalog.h
+++ msm/sde/sde_hw_catalog.h
@@ -56,6 +56,7 @@
 #define SDE_HW_VER_820	SDE_HW_VER(8, 2, 0) /* diwali */
 #define SDE_HW_VER_830	SDE_HW_VER(8, 3, 0) /* parrot */
 #define SDE_HW_VER_850	SDE_HW_VER(8, 5, 0) /* cape */
+#define SDE_HW_VER_860  SDE_HW_VER(8, 6, 0) /* ravelin */
 #define SDE_HW_VER_900	SDE_HW_VER(9, 0, 0) /* kalama */
 #define SDE_HW_VER_A00	SDE_HW_VER(10, 0, 0) /* pineapple */
 
@@ -88,6 +89,7 @@
 #define IS_DIWALI_TARGET(rev) IS_SDE_MAJOR_MINOR_SAME((rev), SDE_HW_VER_820)
 #define IS_PARROT_TARGET(rev) IS_SDE_MAJOR_MINOR_SAME((rev), SDE_HW_VER_830)
 #define IS_CAPE_TARGET(rev) IS_SDE_MAJOR_MINOR_SAME((rev), SDE_HW_VER_850)
+#define IS_RAVELIN_TARGET(rev) IS_SDE_MAJOR_MINOR_SAME((rev), SDE_HW_VER_860)
 #define IS_KALAMA_TARGET(rev) IS_SDE_MAJOR_MINOR_SAME((rev), SDE_HW_VER_900)
 #define IS_PINEAPPLE_TARGET(rev) IS_SDE_MAJOR_MINOR_SAME((rev), SDE_HW_VER_A00)
 
