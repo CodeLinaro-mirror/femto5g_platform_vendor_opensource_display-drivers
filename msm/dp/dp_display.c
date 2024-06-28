f26e88eb1825a9628810b04a7d3090d7d11a1791
Amit Singh <quic_amitsi@quicinc.com>
2024-06-28 10:52:08 -0700

    disp: msm: dp: remove panel power-on sequence during display prepare
    One of the changes done to support eDP panels is triggering panel
    power-on sequence again after successful LT & right before the video
    is sent (Commit SHA: bfe781fd6693992e853dcb967ece9f15b5f6dc0b).
    Triggering this sequence at this stage will put the sink in standby
    state which might cause both aux channel & mainlink to misbehave. This
    is causing regression in few of the usecases where monitor display isn't
    coming up even after successful enabling of the display by the usermode.
    This change removes the power-on sequence invocation during this stage.
    
    Change-Id: Ie16a78b50b4739442c211540f7cdf50112e0d22d
    Signed-off-by: Sandeep Gangadharaiah <quic_sandgang@quicinc.com>
    Signed-off-by: Amit Singh <quic_amitsi@quicinc.com>
    Signed-off-by: Venkatesh Kotha <venkkoth@qti.qualcomm.com>

--- msm/dp/dp_display.c
+++ msm/dp/dp_display.c
@@ -1232,6 +1232,7 @@ static int dp_display_host_ready(struct dp_display_private *dp)
 	dp->ctrl->abort(dp->ctrl, false);
 
 	dp->aux->init(dp->aux, dp->parser->aux_cfg);
+	rc = dp_display_panel_ready(dp);
 
 	dp_display_state_add(DP_STATE_READY);
 	/* log this as it results from user action of cable connection */
@@ -1356,8 +1357,6 @@ static int dp_display_process_hpd_high(struct dp_display_private *dp)
 		dp_display_state_show("[ready failed]");
 		goto err_state;
 	}
-
-	rc = dp_display_panel_ready(dp);
 
 	dp->link->psm_config(dp->link, &dp->panel->link_info, false);
 	dp->debug->psm_enabled = false;
@@ -2540,13 +2539,6 @@ static int dp_display_prepare(struct dp_display *dp_display, void *panel)
 	rc = dp_display_host_ready(dp);
 	if (rc) {
 		dp_display_state_show("[ready failed]");
-		goto end;
-	}
-
-	rc = dp_display_panel_ready(dp);
-	if (rc) {
-		dp_display_host_unready(dp);
-		dp_display_host_deinit(dp);
 		goto end;
 	}
 
@@ -3933,13 +3925,6 @@ static int dp_display_edp_detect(struct dp_display *dp_display)
 	rc = dp_display_host_ready(dp);
 	if (rc) {
 		dp_display_state_show("[ready failed]");
-		dp_display_host_deinit(dp);
-		goto end;
-	}
-
-	rc = dp_display_panel_ready(dp);
-	if (rc) {
-		dp_display_host_unready(dp);
 		dp_display_host_deinit(dp);
 		goto end;
 	}
