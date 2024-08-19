3aa3144410d3baef4ea3349be7230989c83d4754
Soumya Dev <quic_ssoumya@quicinc.com>
2024-08-19 10:53:07 +0530

    disp: msm-hyp: add null check
    This change adds null check at different functions to
    avoid NULL pointer dereference exception.
    
    Change-Id: I35b50cb2194c2fd841fca1fcc0caab3f95105ce2
    Signed-off-by: Soumya Dev <quic_ssoumya@quicinc.com>
    (cherry picked from commit 39f05a00bf7a42cb0c5427c6ab1a4ae102659366)

--- msm-hyp/wfd/wfd_kms.c
+++ msm-hyp/wfd/wfd_kms.c
@@ -592,6 +592,9 @@ bool _wfd_kms_dma_igc_changed(
 	if (!changed)
 		return false;
 
+	if (!dma_config)
+		return false;
+
 	dma_config->bIGCEnabled = cur_en ? WFD_TRUE : WFD_FALSE;
 
 	if (dma_config && cur_en)
@@ -619,6 +622,9 @@ bool _wfd_kms_dma_gc_changed(
 		changed = true;
 
 	if (!changed)
+		return false;
+
+	if (!dma_config)
 		return false;
 
 	dma_config->bGCEnabled= cur_en ? WFD_TRUE : WFD_FALSE;
@@ -650,6 +656,9 @@ bool _wfd_kms_plane_is_3d_gamut_changed(
 		changed = true;
 
 	if (!changed)
+		return false;
+
+	if (!gamut)
 		return false;
 
 	gamut->bGamutEn = cur_en ? WFD_TRUE : WFD_FALSE;
@@ -720,6 +729,9 @@ bool _wfd_kms_plane_is_dma_csc_changed(
 		changed = true;
 
 	if (!changed)
+		return false;
+
+	if (!dma_config)
 		return false;
 
 	dma_config->bCSCEnabled = cur_en ? WFD_TRUE : WFD_FALSE;
@@ -2510,15 +2522,14 @@ static int wfd_kms_remove(struct platform_device *pdev)
 	int buff_idx = 0;
 	struct wire_device *wire_dev = NULL;
 	dma_addr_t *dmabuf_handle = NULL;
-	int export_id;
+	int export_id = 0;
 	void *handle = NULL;
 	struct user_os_utils_mem_info mem = { 0 };
 
 	for (i = 0; i < kms->port_cnt; i++) {
 		for (j = 0; j < kms->pipeline_cnt[i]; j++) {
 			wire_dev = kms->port_devs[i];
-			if (wire_dev)
-				handle = wire_dev->ctx->init_info.context;
+			handle = wire_dev->ctx->init_info.context;
 			wfdSetPipelineAttribiv_User(kms->port_devs[i],kms->pipelines[i][j],
 				WFD_PIPELINE_COLOR_CONFIG_CLEAR, 1, &i);
 			wfdDestroyPipeline_User(kms->port_devs[i], kms->pipelines[i][j]);
