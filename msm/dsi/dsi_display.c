c3dffa149cb0ff1e0118cb0c342ebfd3ca2bfe36
Jinfeng Gu <quic_gjinfeng@quicinc.com>
2024-08-22 15:51:37 +0800

    disp: msm: dsi: add null pointer check in dsi_display_dev_remove
    This change add display null pointer check in dsi_display_dev_remove.
    
    Change-Id: Ib31756c3b22256d19cbcb508f60de4550e3834e1
    Signed-off-by: Jinfeng Gu <quic_gjinfeng@quicinc.com>
    (cherry picked from commit ae1f52783d96828cecd89e9735ffe8e7fd7ec5f1)

--- msm/dsi/dsi_display.c
+++ msm/dsi/dsi_display.c
@@ -6123,6 +6123,10 @@ int dsi_display_dev_remove(struct platform_device *pdev)
 	}
 
 	display = platform_get_drvdata(pdev);
+	if (!display || !display->panel_node) {
+		DSI_ERR("invalid display\n");
+		return -EINVAL;
+	}
 
 	/* decrement ref count */
 	of_node_put(display->panel_node);
