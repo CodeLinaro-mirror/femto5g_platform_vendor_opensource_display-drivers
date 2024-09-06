microsoft/surface-duo-oss-platform.vendor.opensource.display-drivers

disp: msm: dp: fix gpio hpd detection for hpd glitch
Optimize the logic for gpio hpd detection to avoid missing hpd.

Currently when hpd is high and low in a short period of time,
the hpd low will not be detected as gpio detection is switched
to falling edge after hpd is already low.

The new change will always read the hpd state after edge detection
switch, to make sure the next hpd change can be detected.

Change-Id: I2f7c0823e6e37ff8d68257db9e2eae122a8e29a2
Signed-off-by: Xiaowen Wu <wxiaowen@codeaurora.org>

msm/dp/dp_gpio_hpd.c
@@ -13,16 +13,18 @@
 #include <linux/gpio/consumer.h>
 #include <linux/sde_io_util.h>
 #include <linux/of_gpio.h>
+#include <linux/spinlock.h>
 #include "dp_gpio_hpd.h"
 
 struct dp_gpio_hpd_private {
 	struct device *dev;
 	struct dp_hpd base;
 	struct dss_gpio gpio_cfg;
-	struct delayed_work work;
+	struct workqueue_struct *wq;
+	struct work_struct work;
 	struct dp_hpd_cb *cb;
 	int irq;
-	bool hpd;
+	int edge;
 };
 
 static int dp_gpio_hpd_connect(struct dp_gpio_hpd_private *gpio_hpd, bool hpd)
@@ -46,6 +48,8 @@ static int dp_gpio_hpd_connect(struct dp_gpio_hpd_private *gpio_hpd, bool hpd)
 		rc = -EINVAL;
 		goto error;
 	}
+
+	pr_info("%s hpd=%d\n", gpio_hpd->gpio_cfg.gpio_name, hpd);
 
 	if (hpd)
 		rc = gpio_hpd->cb->configure(gpio_hpd->dev);
@@ -78,68 +82,152 @@ error:
 static irqreturn_t dp_gpio_isr(int unused, void *data)
 {
 	struct dp_gpio_hpd_private *gpio_hpd = data;
-	u32 const disconnect_timeout_retry = 50;
-	bool hpd;
-	int i;
 
 	if (!gpio_hpd)
 		return IRQ_NONE;
 
-	hpd = gpio_get_value_cansleep(gpio_hpd->gpio_cfg.gpio);
+	queue_work(gpio_hpd->wq, &gpio_hpd->work);
 
-	if (!gpio_hpd->hpd && hpd) {
-		gpio_hpd->hpd = true;
-		queue_delayed_work(system_wq, &gpio_hpd->work, 0);
-		return IRQ_HANDLED;
-	}
-
-	if (!gpio_hpd->hpd)
-		return IRQ_HANDLED;
-
-	/* In DP 1.2 spec, 100msec is recommended for the detection
-	 * of HPD connect event. Here we'll poll HPD status for
-	 * 50x2ms = 100ms and if HPD is always low, we know DP is
-	 * disconnected. If HPD is high, HPD_IRQ will be handled
-	 */
-	for (i = 0; i < disconnect_timeout_retry; i++) {
-		if (hpd) {
-			dp_gpio_hpd_attention(gpio_hpd);
-			return IRQ_HANDLED;
-		}
-		usleep_range(2000, 2100);
-		hpd = gpio_get_value_cansleep(gpio_hpd->gpio_cfg.gpio);
-	}
-
-	gpio_hpd->hpd = false;
-	queue_delayed_work(system_wq, &gpio_hpd->work, 0);
 	return IRQ_HANDLED;
+}
+
+static inline
+int dp_gpio_set_edge(struct dp_gpio_hpd_private *gpio_hpd, int edge)
+{
+	const int retry_count = 10;
+	int orig_edge = edge;
+	int ret, i;
+	bool hpd;
+
+	if (gpio_hpd->edge == edge)
+		return 0;
+
+	for (i = 0; i < retry_count; i++) {
+		if (gpio_hpd->edge)
+			devm_free_irq(gpio_hpd->dev,
+					gpio_hpd->irq, gpio_hpd);
+
+		ret = devm_request_threaded_irq(gpio_hpd->dev,
+				gpio_hpd->irq, NULL,
+				dp_gpio_isr,
+				edge | IRQF_ONESHOT,
+				"dp-gpio-intp", gpio_hpd);
+
+		if (WARN_ON(ret)) {
+			pr_err("failed to request irq\n");
+			break;
+		}
+
+		gpio_hpd->edge = edge;
+
+		/* check if hpd is changed */
+		hpd = gpio_get_value_cansleep(gpio_hpd->gpio_cfg.gpio);
+
+		if (edge == IRQF_TRIGGER_FALLING) {
+			if (!hpd) {
+				pr_info("hpd bounce to low\n");
+				edge = IRQF_TRIGGER_RISING;
+				continue;
+			}
+			pr_info("%s switch to falling edge\n",
+					gpio_hpd->gpio_cfg.gpio_name);
+		} else {
+			if (hpd) {
+				pr_info("hpd bounce to high\n");
+				edge = IRQF_TRIGGER_FALLING;
+				continue;
+			}
+			pr_info("%s switch to rising edge\n",
+					gpio_hpd->gpio_cfg.gpio_name);
+		}
+
+		break;
+	}
+
+	return (orig_edge != edge);
 }
 
 static void dp_gpio_hpd_work(struct work_struct *work)
 {
-	struct delayed_work *dw = to_delayed_work(work);
-	struct dp_gpio_hpd_private *gpio_hpd = container_of(dw,
+	struct dp_gpio_hpd_private *gpio_hpd = container_of(work,
 		struct dp_gpio_hpd_private, work);
-	int ret;
+	u32 const disconnect_timeout_retry = 50;
+	int ret, i;
+	bool hpd;
 
-	if (gpio_hpd->hpd) {
-		devm_free_irq(gpio_hpd->dev,
-			gpio_hpd->irq, gpio_hpd);
-		ret = devm_request_threaded_irq(gpio_hpd->dev,
-			gpio_hpd->irq, NULL,
-			dp_gpio_isr,
-			IRQF_TRIGGER_FALLING | IRQF_ONESHOT,
-			"dp-gpio-intp", gpio_hpd);
-		dp_gpio_hpd_connect(gpio_hpd, true);
-	} else {
-		devm_free_irq(gpio_hpd->dev,
-				gpio_hpd->irq, gpio_hpd);
-		ret = devm_request_threaded_irq(gpio_hpd->dev,
-			gpio_hpd->irq, NULL,
-			dp_gpio_isr,
-			IRQF_TRIGGER_RISING | IRQF_ONESHOT,
-			"dp-gpio-intp", gpio_hpd);
-		dp_gpio_hpd_connect(gpio_hpd, false);
+	switch (gpio_hpd->edge) {
+	case IRQF_TRIGGER_NONE:
+		hpd = gpio_get_value_cansleep(gpio_hpd->gpio_cfg.gpio);
+		if (hpd) {
+			ret = dp_gpio_set_edge(gpio_hpd, IRQF_TRIGGER_FALLING);
+			if (ret) {
+				pr_info("found hpd high glitch %d\n", ret);
+				break;
+			}
+		} else {
+			ret = dp_gpio_set_edge(gpio_hpd, IRQF_TRIGGER_RISING);
+			if (ret)
+				pr_info("found hpd low glitch %d\n", ret);
+			else
+				break;
+		}
+		ret = dp_gpio_hpd_connect(gpio_hpd, true);
+		break;
+	case IRQF_TRIGGER_RISING:
+		if (gpio_hpd->base.hpd_high)
+			break;
+
+		/*
+		 * According to the DP spec, HPD high event can be confirmed
+		 * only after the HPD line has een asserted continuously for
+		 * more than 100ms
+		 */
+		usleep_range(99000, 100000);
+
+		hpd = gpio_get_value_cansleep(gpio_hpd->gpio_cfg.gpio);
+
+		/* if HPD is low, ignore this event */
+		if (!hpd) {
+			pr_info("ignore false hpd high\n");
+			break;
+		}
+
+		ret = dp_gpio_set_edge(gpio_hpd, IRQF_TRIGGER_FALLING);
+		if (ret) {
+			pr_err("failed to set falling hpd edge %d\n", ret);
+			break;
+		}
+
+		ret = dp_gpio_hpd_connect(gpio_hpd, true);
+		break;
+	case IRQF_TRIGGER_FALLING:
+		if (!gpio_hpd->base.hpd_high)
+			break;
+
+		/* In DP 1.2 spec, 100msec is recommended for the detection
+		 * of HPD connect event. Here we'll poll HPD status for
+		 * 50x2ms = 100ms and if HPD is always low, we know DP is
+		 * disconnected. If HPD is high, HPD_IRQ will be handled
+		 */
+		for (i = 0; i < disconnect_timeout_retry; i++) {
+			hpd = gpio_get_value_cansleep(gpio_hpd->gpio_cfg.gpio);
+			if (hpd) {
+				dp_gpio_hpd_attention(gpio_hpd);
+				return;
+			}
+			usleep_range(2000, 2100);
+		}
+
+		ret = dp_gpio_set_edge(gpio_hpd, IRQF_TRIGGER_RISING);
+		if (ret) {
+			pr_err("failed to set falling hpd edge %d\n", ret);
+			break;
+		}
+
+		ret = dp_gpio_hpd_connect(gpio_hpd, false);
+		break;
+	default:
+		break;
 	}
 
 	if (ret < 0)
@@ -185,30 +273,15 @@ error:
 int dp_gpio_hpd_register(struct dp_hpd *dp_hpd)
 {
 	struct dp_gpio_hpd_private *gpio_hpd;
-	int edge;
-	int rc = 0;
 
 	if (!dp_hpd)
 		return -EINVAL;
 
 	gpio_hpd = container_of(dp_hpd, struct dp_gpio_hpd_private, base);
 
-	gpio_hpd->hpd = gpio_get_value_cansleep(gpio_hpd->gpio_cfg.gpio);
+	queue_work(gpio_hpd->wq, &gpio_hpd->work);
 
-	edge = gpio_hpd->hpd ? IRQF_TRIGGER_FALLING : IRQF_TRIGGER_RISING;
-	rc = devm_request_threaded_irq(gpio_hpd->dev, gpio_hpd->irq, NULL,
-		dp_gpio_isr,
-		edge | IRQF_ONESHOT,
-		"dp-gpio-intp", gpio_hpd);
-	if (rc) {
-		pr_err("Failed to request INTP threaded IRQ: %d\n", rc);
-		return rc;
-	}
-
-	if (gpio_hpd->hpd)
-		queue_delayed_work(system_wq, &gpio_hpd->work, 0);
-
-	return rc;
+	return 0;
 }
 
 struct dp_hpd *dp_gpio_hpd_get(struct device *dev,
@@ -268,7 +341,14 @@ struct dp_hpd *dp_gpio_hpd_get(struct device *dev,
 	gpio_hpd->dev = dev;
 	gpio_hpd->cb = cb;
 	gpio_hpd->irq = gpio_to_irq(gpio_hpd->gpio_cfg.gpio);
-	INIT_DELAYED_WORK(&gpio_hpd->work, dp_gpio_hpd_work);
+
+	gpio_hpd->wq = create_singlethread_workqueue("dp-gpio-hpd-wq");
+	if (!gpio_hpd->wq) {
+		pr_err("failed to create work\n");
+		goto gpio_error;
+	}
+
+	INIT_WORK(&gpio_hpd->work, dp_gpio_hpd_work);
 
 	gpio_hpd->base.simulate_connect = dp_gpio_hpd_simulate_connect;
 	gpio_hpd->base.simulate_attention = dp_gpio_hpd_simulate_attention;

CodeLinaro-mirror/femto5g_platform_vendor_opensource_display-drivers

Signed-off-by: huiyang <quic_huiyang@quicinc.com>
(cherry picked from commit 84895d9f0c4bde12c680d0d2ab74486183ae97fa)

msm/dp/dp_gpio_hpd.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
- * Copyright (c) 2021-2023, Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2021-2024, Qualcomm Innovation Center, Inc. All rights reserved.
  * Copyright (c) 2016-2019, The Linux Foundation. All rights reserved.
  */
 
@@ -21,14 +21,15 @@ struct dp_gpio_hpd_private {
 	struct dp_hpd base;
 	struct dp_parser *parser;
 	struct dss_gpio gpio_cfg;
-	struct delayed_work work;
+	struct workqueue_struct *wq;
+	struct work_struct work;
 	struct workqueue_struct *connect_wq;
 	struct work_struct connect;
 	struct work_struct disconnect;
 	struct work_struct attention;
 	struct dp_hpd_cb *cb;
 	int irq;
-	bool hpd;
+	int edge;
 };
 
 static void dp_gpio_hpd_attention_work(struct work_struct *work)
@@ -101,6 +102,7 @@ static int dp_gpio_hpd_connect(struct dp_gpio_hpd_private *gpio_hpd, bool hpd)
 		goto error;
 	}
 
+	DP_DEBUG("%s hpd=%d\n", gpio_hpd->gpio_cfg.gpio_name, hpd);
 	if (hpd) {
 		rc = queue_work(gpio_hpd->connect_wq,
 				&gpio_hpd->connect);
@@ -147,68 +149,153 @@ error:
 static irqreturn_t dp_gpio_isr(int unused, void *data)
 {
 	struct dp_gpio_hpd_private *gpio_hpd = data;
-	u32 const disconnect_timeout_retry = 50;
-	bool hpd;
-	int i;
 
 	if (!gpio_hpd)
 		return IRQ_NONE;
 
-	hpd = gpio_get_value_cansleep(gpio_hpd->gpio_cfg.gpio);
+	queue_work(gpio_hpd->wq, &gpio_hpd->work);
 
-	if (!gpio_hpd->hpd && hpd) {
-		gpio_hpd->hpd = true;
-		queue_delayed_work(system_wq, &gpio_hpd->work, 0);
-		return IRQ_HANDLED;
-	}
-
-	if (!gpio_hpd->hpd)
-		return IRQ_HANDLED;
-
-	/* In DP 1.2 spec, 100msec is recommended for the detection
-	 * of HPD connect event. Here we'll poll HPD status for
-	 * 50x2ms = 100ms and if HPD is always low, we know DP is
-	 * disconnected. If HPD is high, HPD_IRQ will be handled
-	 */
-	for (i = 0; i < disconnect_timeout_retry; i++) {
-		if (hpd) {
-			dp_gpio_hpd_attention(gpio_hpd);
-			return IRQ_HANDLED;
-		}
-		usleep_range(2000, 2100);
-		hpd = gpio_get_value_cansleep(gpio_hpd->gpio_cfg.gpio);
-	}
-
-	gpio_hpd->hpd = false;
-	queue_delayed_work(system_wq, &gpio_hpd->work, 0);
 	return IRQ_HANDLED;
+}
+
+static inline
+int dp_gpio_set_edge(struct dp_gpio_hpd_private *gpio_hpd, int edge)
+{
+	const int retry_count = 10;
+	int orig_edge = edge;
+	int ret, i;
+	bool hpd;
+
+	if (gpio_hpd->edge == edge)
+		return 0;
+
+	for (i = 0; i < retry_count; i++) {
+		if (gpio_hpd->edge)
+			devm_free_irq(gpio_hpd->dev,
+					gpio_hpd->irq, gpio_hpd);
+
+		ret = devm_request_threaded_irq(gpio_hpd->dev,
+				gpio_hpd->irq, NULL,
+				dp_gpio_isr,
+				edge | IRQF_ONESHOT,
+				"dp-gpio-intp", gpio_hpd);
+
+		if (WARN_ON(ret)) {
+			DP_ERR("failed to request irq\n");
+			break;
+		}
+
+		gpio_hpd->edge = edge;
+
+		/* check if hpd is changed */
+		hpd = gpio_get_value_cansleep(gpio_hpd->gpio_cfg.gpio);
+
+		if (edge == IRQF_TRIGGER_FALLING) {
+			if (!hpd) {
+				DP_INFO("hpd bounce to low\n");
+				edge = IRQF_TRIGGER_RISING;
+				continue;
+			}
+			DP_INFO("%s switch to falling edge\n",
+					gpio_hpd->gpio_cfg.gpio_name);
+		} else {
+			if (hpd) {
+				DP_INFO("hpd bounce to high\n");
+				edge = IRQF_TRIGGER_FALLING;
+				continue;
+			}
+			DP_INFO("%s switch to rising edge\n",
+					gpio_hpd->gpio_cfg.gpio_name);
+		}
+
+		break;
+	}
+
+	return (orig_edge != edge);
 }
 
 static void dp_gpio_hpd_work(struct work_struct *work)
 {
-	struct delayed_work *dw = to_delayed_work(work);
-	struct dp_gpio_hpd_private *gpio_hpd = container_of(dw,
+	struct dp_gpio_hpd_private *gpio_hpd = container_of(work,
 		struct dp_gpio_hpd_private, work);
-	int ret;
+	u32 const disconnect_timeout_retry = 50;
+	int ret = 0;
+	int i;
+	bool hpd;
 
-	if (gpio_hpd->hpd) {
-		devm_free_irq(gpio_hpd->dev,
-			gpio_hpd->irq, gpio_hpd);
-		ret = devm_request_threaded_irq(gpio_hpd->dev,
-			gpio_hpd->irq, NULL,
-			dp_gpio_isr,
-			IRQF_TRIGGER_FALLING | IRQF_ONESHOT,
-			"dp-gpio-intp", gpio_hpd);
-		dp_gpio_hpd_connect(gpio_hpd, true);
-	} else {
-		devm_free_irq(gpio_hpd->dev,
-				gpio_hpd->irq, gpio_hpd);
-		ret = devm_request_threaded_irq(gpio_hpd->dev,
-			gpio_hpd->irq, NULL,
-			dp_gpio_isr,
-			IRQF_TRIGGER_RISING | IRQF_ONESHOT,
-			"dp-gpio-intp", gpio_hpd);
-		dp_gpio_hpd_connect(gpio_hpd, false);
+	switch (gpio_hpd->edge) {
+	case IRQF_TRIGGER_NONE:
+		hpd = gpio_get_value_cansleep(gpio_hpd->gpio_cfg.gpio);
+		if (hpd) {
+			ret = dp_gpio_set_edge(gpio_hpd, IRQF_TRIGGER_FALLING);
+			if (ret) {
+				DP_INFO("found hpd high glitch %d\n", ret);
+				break;
+			}
+		} else {
+			ret = dp_gpio_set_edge(gpio_hpd, IRQF_TRIGGER_RISING);
+			if (ret)
+				DP_INFO("found hpd low glitch %d\n", ret);
+			else
+				break;
+		}
+		ret = dp_gpio_hpd_connect(gpio_hpd, true);
+		break;
+	case IRQF_TRIGGER_RISING:
+		if (gpio_hpd->base.hpd_high)
+			break;
+
+		/*
+		 * According to the DP spec, HPD high event can be confirmed
+		 * only after the HPD line has een asserted continuously for
+		 * more than 100ms
+		 */
+		usleep_range(99000, 100000);
+
+		hpd = gpio_get_value_cansleep(gpio_hpd->gpio_cfg.gpio);
+
+		/* if HPD is low, ignore this event */
+		if (!hpd) {
+			DP_INFO("ignore false hpd high\n");
+			break;
+		}
+
+		ret = dp_gpio_set_edge(gpio_hpd, IRQF_TRIGGER_FALLING);
+		if (ret) {
+			DP_ERR("failed to set falling hpd edge %d\n", ret);
+			break;
+		}
+
+		ret = dp_gpio_hpd_connect(gpio_hpd, true);
+		break;
+	case IRQF_TRIGGER_FALLING:
+		if (!gpio_hpd->base.hpd_high)
+			break;
+
+		/* In DP 1.2 spec, 100msec is recommended for the detection
+		 * of HPD connect event. Here we'll poll HPD status for
+		 * 50x2ms = 100ms and if HPD is always low, we know DP is
+		 * disconnected. If HPD is high, HPD_IRQ will be handled
+		 */
+		for (i = 0; i < disconnect_timeout_retry; i++) {
+			hpd = gpio_get_value_cansleep(gpio_hpd->gpio_cfg.gpio);
+			if (hpd) {
+				dp_gpio_hpd_attention(gpio_hpd);
+				return;
+			}
+			usleep_range(2000, 2100);
+		}
+
+		ret = dp_gpio_set_edge(gpio_hpd, IRQF_TRIGGER_RISING);
+		if (ret) {
+			DP_ERR("failed to set falling hpd edge %d\n", ret);
+			break;
+		}
+
+		ret = dp_gpio_hpd_connect(gpio_hpd, false);
+		break;
+	default:
+		break;
 	}
 
 	if (ret < 0)
@@ -266,17 +353,12 @@ static void dp_gpio_hpd_host_init(struct dp_hpd *dp_hpd,
 	gpio_hpd = container_of(dp_hpd, struct dp_gpio_hpd_private, base);
 
 	hpd = gpio_get_value_cansleep(gpio_hpd->gpio_cfg.gpio);
-	if (gpio_hpd->hpd != hpd) {
-		DP_INFO("DP%d HPD changes during suspension %d->%d\n",
-				gpio_hpd->parser->cell_idx, gpio_hpd->hpd, hpd);
-		gpio_hpd->hpd = hpd;
-	}
 
-	if (gpio_hpd->hpd != gpio_hpd->base.hpd_high) {
+	if (hpd != gpio_hpd->base.hpd_high) {
 		DP_INFO("DP%d missed HPD %s edge?\n",
 				gpio_hpd->parser->cell_idx,
-				gpio_hpd->hpd ? "raising" : "falling");
-		queue_delayed_work(system_wq, &gpio_hpd->work, 0);
+				hpd ? "raising" : "falling");
+		queue_work(gpio_hpd->wq, &gpio_hpd->work);
 	}
 }
 
@@ -298,31 +380,15 @@ static void dp_gpio_hpd_host_deinit(struct dp_hpd *dp_hpd,
 int dp_gpio_hpd_register(struct dp_hpd *dp_hpd)
 {
 	struct dp_gpio_hpd_private *gpio_hpd;
-	int edge;
-	int rc = 0;
 
 	if (!dp_hpd)
 		return -EINVAL;
 
 	gpio_hpd = container_of(dp_hpd, struct dp_gpio_hpd_private, base);
 
-	gpio_hpd->hpd = gpio_get_value_cansleep(gpio_hpd->gpio_cfg.gpio);
+	queue_work(gpio_hpd->wq, &gpio_hpd->work);
 
-	edge = gpio_hpd->hpd ? IRQF_TRIGGER_FALLING : IRQF_TRIGGER_RISING;
-	rc = devm_request_threaded_irq(gpio_hpd->dev, gpio_hpd->irq, NULL,
-		dp_gpio_isr,
-		edge | IRQF_ONESHOT,
-		"dp-gpio-intp", gpio_hpd);
-	if (rc) {
-		DP_ERR("DP%d Failed to request INTP threaded IRQ: %d\n",
-				gpio_hpd->parser->cell_idx, rc);
-		return rc;
-	}
-
-	if (gpio_hpd->hpd)
-		queue_delayed_work(system_wq, &gpio_hpd->work, 0);
-
-	return rc;
+	return 0;
 }
 
 struct dp_hpd *dp_gpio_hpd_get(struct device *dev,
@@ -385,7 +451,14 @@ struct dp_hpd *dp_gpio_hpd_get(struct device *dev,
 	gpio_hpd->parser = parser;
 	gpio_hpd->cb = cb;
 	gpio_hpd->irq = gpio_to_irq(gpio_hpd->gpio_cfg.gpio);
-	INIT_DELAYED_WORK(&gpio_hpd->work, dp_gpio_hpd_work);
+
+	gpio_hpd->wq = create_singlethread_workqueue("dp-gpio-hpd-wq");
+	if (!gpio_hpd->wq) {
+		DP_ERR("failed to create work\n");
+		goto gpio_error;
+	}
+
+	INIT_WORK(&gpio_hpd->work, dp_gpio_hpd_work);
 
 	gpio_hpd->connect_wq = create_singlethread_workqueue("dp-gpio-conn-eq");
 	if (!gpio_hpd->connect_wq) {