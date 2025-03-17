2284bdc7b3d8f0ee5eb7eb50cd2821ccc390a9ed
Kirill Shpin <quic_kshpin@quicinc.com>
2025-03-17 16:34:24 -0700

    disp: msm: sde: downgrade non-fatal log to info
    During bootup, driver attempts to send a frequency pattern update to
    the panel before the command parsing occurs. This isn't fatal, and only
    happens in this scenario, so downgrade the log to info.
    
    Change-Id: I1ba31f8fd7725c2d06f50f7031a4fa02dd7ad783
    Signed-off-by: Kirill Shpin <quic_kshpin@quicinc.com>

--- msm/sde/sde_connector.c
+++ msm/sde/sde_connector.c
@@ -1416,7 +1416,8 @@ int sde_connector_check_update_vhm_cmd(struct drm_connector *connector)
 		return 0;
 
 	if (!c_conn->freq_pattern) {
-		SDE_ERROR("frequency pattern is NULL but update is true\n");
+		SDE_INFO("frequency pattern is NULL but update is true\n");
+		SDE_EVT32(SDE_EVTLOG_FUNC_CASE3, SDE_EVTLOG_ERROR);
 		return -EINVAL;
 	}
 
