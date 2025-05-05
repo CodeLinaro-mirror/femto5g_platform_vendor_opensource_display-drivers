2ccce85da94861173bbd958e8afc4551b18c7bce
Prabhanjan Kandula <quic_pkandula@quicinc.com>
2025-05-05 16:11:05 -0700

    msm: hfi: add hfi property for commit sequence counter
    This change introduces hfi property for clients to provide
    a commit sequence counter and FW would provide this frame
    counter with scan-start event response once the corresponding
    frame is picked up by DPU HW.
    
    Change-Id: I35b35a1c963b624e86e895f53d8f7948115088cb
    Signed-off-by: Prabhanjan Kandula <quic_pkandula@quicinc.com>

--- msm/hfi/hfi_properties_display.h
+++ msm/hfi/hfi_properties_display.h
@@ -371,6 +371,20 @@
 #define HFI_PROPERTY_DISPLAY_DETACH_LAYER                            0x00020019
 
 /*
+ * HFI_PROPERTY_DISPLAY_SCAN_SEQUENCE_ID - This property is to add a 'sequence number' identifier
+ *                                         to a commit that DCP can return along with the
+ *                                         HFI_COMMAND_DISPLAY_EVENT_FRAME_SCAN_START event.
+ *                                         Host is expected to send this packet as part of
+ *                                         HFI_COMMAND_DISPLAY_SET_PROPERTY command packet payload.
+ *
+ * @BasicFuntionality - HFI_PROPERTY_DISPLAY_SCAN_SEQUENCE_ID
+ * (u32_key) payload [0]     : HFI_PROPERTY_DISPLAY_SCAN_SEQUENCE_ID |
+ *                             (version=0 << 20) | (dsize=1 << 24 )
+ * (u32_value) payload [1]     : sequence_id
+ */
+#define HFI_PROPERTY_DISPLAY_SCAN_SEQUENCE_ID                        0x0002001A
+
+/*
  * All display color properties begin here
  */
 #define HFI_PROPERTY_DISPLAY_COLOR_BEGIN                             0x00020100
