ee42f53b8f78b0513902639bbfe2e23d6f548c6a
Jayaprakash Madisetty <quic_jmadiset@quicinc.com>
2025-05-08 06:56:50 +0530

    disp: msm: fix display drivers code compilation in LE
    Add changes to allow display hfi code to be compiled
    with CONFIG_QTI_HFI_CORE when mm_drivers_headers are
    not exported.
    
    Change-Id: I4050d67b6f8b68b8a5f124af7a87456715664fe5
    Signed-off-by: Jayaprakash Madisetty <quic_jmadiset@quicinc.com>

--- msm/hfi/hfi_adapter.h
+++ msm/hfi/hfi_adapter.h
@@ -14,7 +14,9 @@
 #include <linux/spinlock.h>
 #if IS_ENABLED(CONFIG_MDSS_HFI_ADAPTER)
 #include "hfi_pack_unpack_common.h"
+#if IS_ENABLED(CONFIG_QTI_HFI_CORE)
 #include "hfi_interface.h"
+#endif
 #include "hfi_packer.h"
 #include "hfi_unpacker.h"
 #endif
@@ -141,7 +143,7 @@ struct hfi_cmdbuf_t {
 	u32 unique_id;
 	u32 obj_id;
 	u32 size;
-#if IS_ENABLED(CONFIG_MDSS_HFI_ADAPTER)
+#if IS_ENABLED(CONFIG_QTI_HFI_CORE)
 	struct hfi_core_cmds_buf_desc buf;
 #endif
 	struct list_head node;
@@ -221,10 +223,12 @@ struct msm_dbg_addr_map {
 	void __iomem *local_addr;
 	u32 size;
 	u32 aligned_size;
+#if IS_ENABLED(CONFIG_QTI_HFI_CORE)
 	struct hfi_core_mem_alloc_info alloc_info;
+#endif
 };
 
-#if IS_ENABLED(CONFIG_MDSS_HFI_ADAPTER)
+#if IS_ENABLED(CONFIG_QTI_HFI_CORE)
 
 /**
  * hfi_adapter_init - Creates HFI adapter module object to connect with HFI driver.
@@ -346,7 +350,6 @@ void hfi_adapter_buffer_alloc(struct msm_dbg_addr_map *addr_map);
  * and pointers to kernel & hfi address of the shared space.
  */
 void hfi_adapter_buffer_dealloc(struct hfi_core_mem_alloc_info *alloc_info);
-
 #else
 
 static inline struct hfi_adapter_t *hfi_adapter_init(int instance)
@@ -412,10 +415,13 @@ static inline void hfi_adapter_buffer_alloc(struct msm_dbg_addr_map *addr_map)
 
 }
 
+#if IS_ENABLED(CONFIG_QTI_HFI_CORE)
 static inline void hfi_adapter_buffer_dealloc(struct hfi_core_mem_alloc_info *alloc_info)
 {
 
 }
 #endif
+
+#endif /*#if IS_ENABLED(CONFIG_QTI_HFI_CORE)*/
 
 #endif  /* _HFI_ADAPTER_H_ */
