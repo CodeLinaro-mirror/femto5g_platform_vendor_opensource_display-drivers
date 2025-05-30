424a0c8f757d5b555c2cf1eb9bc234027c1fd281
Ramaprasad Nagaraj <quic_ramapras@quicinc.com>
2025-05-30 17:58:39 +0530

    display: msm-hyp: Initialized uninitialized variables
    Prevent potential issues initialize the uninitialized variables.
    
    Change-Id: Iaba30a6fae291d424c128ff17dabc9a4deb7bd4c
    Signed-off-by: Ramaprasad Nagaraj <quic_ramapras@quicinc.com>

--- msm-hyp/virtio/virtio_kms.c
+++ msm-hyp/virtio/virtio_kms.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
- * Copyright (c) 2023-2025 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  */
 #include <linux/sort.h>
 #include <drm/drm_atomic.h>
@@ -1450,7 +1450,7 @@ static void virtio_check_framebuffer_contents(struct dma_buf *dma_buf_dump)
 {
 	int ret = 0;
 	char *ptr;
-	struct iosys_map map;
+	struct iosys_map map = {0};
 
 	dma_buf_begin_cpu_access(dma_buf_dump, DMA_BIDIRECTIONAL);
 
