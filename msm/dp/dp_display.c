6e37558808f9c6027070e3be02fe7f122773783f
Haihua Nie <nie@qti.qualcomm.com>
2025-06-07 17:13:40 +0800

    disp: dp: add HOTPLUG=1 in hotplug uevent message about drm-backend
    Add HOTPLUG=1 in hotplug uevent message to align with upstream
    hotplug helper about drm-backend.
    
    Change-Id: I00eb369831584e27775b07362493d3c47107f4a4
    Signed-off-by: Haihua Nie <nie@qti.qualcomm.com>

--- msm/dp/dp_display.c
+++ msm/dp/dp_display.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
- * Copyright (c) 2021-2024, Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  * Copyright (c) 2017-2021, The Linux Foundation. All rights reserved.
  */
 
@@ -914,7 +914,8 @@ static bool dp_display_send_hpd_event(struct dp_display_private *dp)
 	struct drm_connector *connector;
 	char name[HPD_STRING_SIZE], status[HPD_STRING_SIZE],
 		bpp[HPD_STRING_SIZE], pattern[HPD_STRING_SIZE];
-	char *envp[5];
+	char *envp[6];
+	char *event_string = "HOTPLUG=1";
 	struct dp_display *display;
 	int rc = 0;
 
@@ -950,7 +951,8 @@ static bool dp_display_send_hpd_event(struct dp_display_private *dp)
 	envp[1] = status;
 	envp[2] = bpp;
 	envp[3] = pattern;
-	envp[4] = NULL;
+	envp[4] = event_string;
+	envp[5] = NULL;
 
 	rc = kobject_uevent_env(&dev->primary->kdev->kobj, KOBJ_CHANGE, envp);
 	DP_INFO("uevent %s: %d\n", rc ? "failure" : "success", rc);
