6442b7815f607fc1a2ffdde19fe0efb8448b0101
Tanmay Karnavat <quic_tkarnava@quicinc.com>
2025-06-24 13:29:06 -0700

    disp: msm: sde: update poms during transition
    During poms transition ALIGN_VSYNC_TO_TE needs to
    be set to true andit is not applicable during
    suspend resume.
    
    Change-Id: I180ff6f6ad3f95c4ee042c66f49a6310406641ff
    Signed-off-by: Tanmay Karnavat <quic_tkarnava@quicinc.com>
    Signed-off-by: Valentine Ssebuyungo <quic_vssebuyu@quicinc.com>

--- msm/sde/sde_hw_intf.h
+++ msm/sde/sde_hw_intf.h
@@ -39,6 +39,7 @@ struct intf_timing_params {
 	u32 extra_dto_cycles;	/* for DP only */
 	bool dsc_4hs_merge;	/* DSC 4HS merge */
 	bool poms_align_vsync;	/* poms with vsync aligned */
+	bool poms_pending;
 	u32 dce_bytes_per_line;
 	u32 vrefresh;
 };
