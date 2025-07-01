bfe625bfb314be387d4be685ca7176e422ce5eb9
Grace An <gracan@qti.qualcomm.com>
2025-07-01 11:24:08 -0700

    disp: msm: sde: clean up hw-fence disablement due to failures
    Change SDE_DEBUG to SDE_INFO or SDE_ERROR messages as appropriate when
    hw-fence is disabled due to hw-fence driver unavailability or ipcc
    mapping failures. Move hw-fence disablement due to power vote failure
    to earlier in function call to avoid incorrectly setting
    is_vrr_hw_fence_enable to true when hw-fences should be disabled.
    
    Change-Id: I2242626f9e1bd4b28dc1d31092db079cba636c78
    Signed-off-by: Grace An <gracan@qti.qualcomm.com>

--- msm/sde/sde_hw_catalog.c
+++ msm/sde/sde_hw_catalog.c
@@ -4608,8 +4608,11 @@ static void _sde_top_parse_dt_helper(struct sde_mdss_cfg *cfg,
 	cfg->ipcc_protocol_offset = PROP_VALUE_ACCESS(props->values, IPCC_PROTOCOL_OFFSET, 0);
 	if (!cfg->ipcc_protocol_offset)
 		cfg->ipcc_protocol_offset = HW_FENCE_DEFAULT_IPCC_PROTOCOL_OFFSET;
-	if (!cfg->ipcc_protocol_id || !cfg->ipcc_client_phys_id)
-		cfg->hw_fence_rev = 0; /* disable hw fences*/
+	if (!cfg->ipcc_protocol_id || !cfg->ipcc_client_phys_id) {
+		SDE_INFO("disabling hw-fence because invalid protocol_id:%d client_phys_id:%d\n",
+			cfg->ipcc_protocol_id, cfg->ipcc_client_phys_id);
+		cfg->hw_fence_rev = 0;
+	}
 	cfg->ipcc_client_out_phys_id = PROP_VALUE_ACCESS(props->values, IPCC_CLIENT_OUT_PHYS_ID, 0);
 
 	cfg->soccp_ph = PROP_VALUE_ACCESS(props->values, SOCCP_PH, 0);
