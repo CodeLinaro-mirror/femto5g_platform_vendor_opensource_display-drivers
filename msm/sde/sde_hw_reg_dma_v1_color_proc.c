64f33b7899c2119f31d4958c25d86f460afd3452
Renchao Liu <rencliu@qti.qualcomm.com>
2025-07-17 17:17:59 +0800

    disp: msm: Fix NULL check order issues
    Change fixes few NULL check order issues.
    
    Change-Id: Ib9c3f5b1310134ae860abf91249b48fb7bc8853d
    Signed-off-by: Renchao Liu <rencliu@qti.qualcomm.com>

--- msm/sde/sde_hw_reg_dma_v1_color_proc.c
+++ msm/sde/sde_hw_reg_dma_v1_color_proc.c
@@ -1847,8 +1847,8 @@ int reg_dmav1_setup_rc_mask_configv1(struct sde_hw_dspp *ctx, void *cfg)
 	} else {
 		SDE_DEBUG("partial frame update\n");
 		sde_kms_rect_merge_rectangles(last_roi_list, &merged_roi);
+		SDE_EVT32(RC_IDX(ctx), last_roi_list->num_rects);
 	}
-	SDE_EVT32(RC_IDX(ctx), last_roi_list->num_rects);
 
 	rc = _sde_hw_rc_get_ajusted_roi(hw_cfg, &merged_roi, &rc_roi);
 	if (rc) {
