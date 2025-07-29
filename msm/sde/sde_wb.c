c35e4a23de57f030307f9f6c1a3bc516c643100d
Hemant Jain <hemajain@qti.qualcomm.com>
2025-07-29 11:04:00 +0800

    disp: msm: sde: Add change to fix slab out of bounds
    Non null terminated string from user space can cause out
    of bound access issue. Hence added a NULL character
    explicitly in name when received from user space.
    
    Change-Id: I08095a40a78d7bd023849d83ed8a5440cd763dd8
    Signed-off-by: Hemant Jain <hemajain@qti.qualcomm.com>

--- msm/sde/sde_wb.c
+++ msm/sde/sde_wb.c
@@ -1,7 +1,6 @@
-// SPDX-License-Identifier: GPL-2.0-only
-/*
- * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries. All rights reserved.
- * Confidential and Proprietary - Qualcomm Technologies, Inc.
+/*Copyright (c) 2015-2021, The Linux Foundation. All rights reserved.
+ *Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ *SPDX-License-Identifier: GPL-2.0-only
  */
 
 #define pr_fmt(fmt)	"[drm:%s:%d] " fmt, __func__, __LINE__
