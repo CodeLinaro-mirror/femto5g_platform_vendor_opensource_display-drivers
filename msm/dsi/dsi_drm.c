6082d752b9cb248cabc97d260649e2251c7d3b7d
zijizhou <quic_zijizhou@quicinc.com>
2025-07-31 16:49:54 +0800

    disp: msm: add support for max modes to 256
    This change increases the maximum number of supported display modes to
    256.The single u32 allowed_mode_switch field is replaced with a bitmap
    array, where each bit represents whether a mode switch is allowed.
    
    Key changes:
    - Introduce MODE_SWITCH_BITMAP_SIZE and related macros to
      manage bitmap.
    - Support bitmap array in set_allowed_mode_switch for extended
      mode range.
    - Increased KMS info size to accommodate up to 256 DSI modes.
    
    Change-Id: I54ce09b5c24f7e9bb9dacd4018bc6f1e5094f32c
    Signed-off-by: zijizhou <quic_zijizhou@quicinc.com>

--- msm/dsi/dsi_drm.c
+++ msm/dsi/dsi_drm.c
@@ -745,8 +745,8 @@ int dsi_conn_get_mode_info(struct drm_connector *connector,
 			sizeof(dsi_mode->priv_info->roi_caps));
 	}
 
-	mode_info->allowed_mode_switches =
-		dsi_mode->priv_info->allowed_mode_switch;
+	memcpy(mode_info->allowed_mode_switches, dsi_mode->priv_info->allowed_mode_switch,
+			sizeof(mode_info->allowed_mode_switches));
 
 	return 0;
 }
@@ -1560,6 +1560,14 @@ static bool is_valid_poms_switch(struct dsi_display_mode *mode_a,
 			(mode_a->timing.h_active == mode_b->timing.h_active));
 }
 
+static inline void set_allowed_mode_switch_bit(uint32_t *bitmap_array, int mode_idx)
+{
+	int arr_idx = mode_idx / MODE_SWITCH_BITS_PER_WORD;
+	int bit_idx = mode_idx % MODE_SWITCH_BITS_PER_WORD;
+
+	bitmap_array[arr_idx] |= BIT(bit_idx);
+}
+
 void dsi_conn_set_allowed_mode_switch(struct drm_connector *connector,
 		void *display)
 {
@@ -1592,12 +1600,13 @@ void dsi_conn_set_allowed_mode_switch(struct drm_connector *connector,
 			return;
 
 		dsi_mode_info =  panel_dsi_mode->priv_info;
-		dsi_mode_info->allowed_mode_switch |= BIT(mode_idx);
+		set_allowed_mode_switch_bit(dsi_mode_info->allowed_mode_switch, mode_idx);
+
 		if (mode_idx == mode_count - 1)
 			break;
 
 		mode_list = mode_list->next;
-		cmp_mode_idx = 1;
+		cmp_mode_idx = mode_idx + 1;
 		list_for_each_entry(cmp_drm_mode, mode_list, head) {
 			if (&cmp_drm_mode->head == &connector->modes)
 				continue;
@@ -1633,13 +1642,13 @@ void dsi_conn_set_allowed_mode_switch(struct drm_connector *connector,
 			}
 
 			if (allow_switch) {
-				dsi_mode_info->allowed_mode_switch |=
-					BIT(mode_idx + cmp_mode_idx);
-				cmp_dsi_mode_info->allowed_mode_switch |=
-					BIT(mode_idx);
+				set_allowed_mode_switch_bit(dsi_mode_info->allowed_mode_switch,
+					cmp_mode_idx);
+				set_allowed_mode_switch_bit(cmp_dsi_mode_info->allowed_mode_switch,
+					mode_idx);
 			}
 
-			if ((mode_idx + cmp_mode_idx) >= mode_count - 1)
+			if (cmp_mode_idx == mode_count - 1)
 				break;
 
 			cmp_mode_idx++;
--- msm/msm_drv.h
+++ msm/msm_drv.h
@@ -83,6 +83,14 @@ struct msm_gem_vma;
 #define MAX_BRIDGES    16
 #define MAX_CONNECTORS 16
 
+#if IS_ENABLED(CONFIG_DSI_EXTENDED_MODES)
+#define DSI_MODE_MAX 256
+#else
+#define DSI_MODE_MAX 32
+#endif
+#define MODE_SWITCH_BITS_PER_WORD 32
+#define MODE_SWITCH_BITMAP_SIZE (DSI_MODE_MAX / MODE_SWITCH_BITS_PER_WORD)
+
 #define MSM_RGB 0x0
 #define MSM_YUV 0x1
 
@@ -1017,7 +1025,7 @@ struct msm_mode_info {
 	u32 mdp_transfer_time_us;
 	u32 mdp_transfer_time_us_min;
 	u32 mdp_transfer_time_us_max;
-	u32 allowed_mode_switches;
+	u32 allowed_mode_switches[MODE_SWITCH_BITMAP_SIZE];
 	bool disable_rsc_solver;
 	struct msm_dyn_clk_list dyn_clk_list;
 	struct msm_freq_step_list *freq_step_list;
