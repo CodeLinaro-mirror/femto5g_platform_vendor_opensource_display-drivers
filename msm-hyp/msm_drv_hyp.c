b61a53689c7a4a3bbe668996c7a4832231825756
Hemant Jain <hemajain@qti.qualcomm.com>
2025-08-11 02:00:49 -0700

    disp: msm-hyp: Revert support for NV21 pixel format
    Revert support for NV21 format in LAGVM for wfd_kms driver
    
    Change-Id: I33413d24f1b965bebe44e9f092d652ba985ecb6a
    Signed-off-by: Hemant Jain <hemajain@qti.qualcomm.com>

--- msm-hyp/msm_drv_hyp.c
+++ msm-hyp/msm_drv_hyp.c
@@ -452,9 +452,10 @@ static const struct backlight_ops msm_hyp_backlight_device_ops = {
 
 static int _msm_hyp_connector_create_backlight(struct msm_hyp_connector *connector)
 {
-	struct backlight_properties props = {0};
+	struct backlight_properties props;
 	char bl_node_name[32];
 
+	memset(&props, 0, sizeof(props));
 	props.type = BACKLIGHT_RAW;
 	props.power = FB_BLANK_UNBLANK;
 	props.max_brightness = 255;
@@ -1579,8 +1580,7 @@ static bool msm_hyp_plane_format_mod_supported(struct drm_plane *plane,
 			format == DRM_FORMAT_XRGB2101010 ||
 			format == DRM_FORMAT_ABGR2101010 ||
 			format == DRM_FORMAT_XBGR2101010 ||
-			format == DRM_FORMAT_BGR888 ||
-			format == DRM_FORMAT_NV21) {
+			format == DRM_FORMAT_BGR888) {
 			/* Linear formats */
 			ret = true;
 		}
