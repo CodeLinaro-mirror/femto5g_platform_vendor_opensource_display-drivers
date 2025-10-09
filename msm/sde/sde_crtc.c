95988443dfea4a89efb7a2eaf1f7071ec5e4d4e4
Srinivas Rao Daru <sdaru@qti.qualcomm.com>
2025-10-09 12:02:21 -0700

    disp: msm: add support for OPR and MISR event in HLOS
    - Adds support for OPR and MISR event register and deregister from HLOS
    - Adds support for getting OPR and MISR values from FW
      and then sending back to user space
    - Adds support for setting MISR property
    
    This supports both OPR and MISR event.
    
    Change-Id: I6056f3be5e5269a3d6363c504df3c5b5555f7a1f
    Signed-off-by: Srinivas Rao Daru <sdaru@qti.qualcomm.com>

--- msm/sde/sde_crtc.c
+++ msm/sde/sde_crtc.c
@@ -3821,31 +3821,58 @@ static void _sde_crtc_retire_event(struct drm_connector *connector,
 	SDE_ATRACE_END("signal_retire_fence");
 }
 
-void sde_crtc_opr_event_notify(struct drm_crtc *crtc)
+void sde_crtc_opr_event_notify(struct drm_crtc *crtc, void *args)
 {
 	struct sde_crtc *sde_crtc;
 	uint32_t current_opr_value[MAX_DSI_DISPLAYS] = {0};
 	int i, rc;
 	bool updated = false;
 	struct drm_event event;
-
+	enum msm_disp_op disp_op = sde_crtc_get_disp_op(crtc);
 	sde_crtc = to_sde_crtc(crtc);
+	u32 *opr_data;
 
 	atomic_set(&sde_crtc->previous_opr_value.num_valid_opr, 0);
-	for (i = 0; i < sde_crtc->num_mixers; i++) {
-		rc = sde_dspp_spr_read_opr_value(sde_crtc->mixers[i].hw_dspp,
-			&current_opr_value[i]);
-		if (rc) {
-			SDE_ERROR("failed to collect OPR idx: %d rc: %d\n", i, rc);
-			continue;
+
+	if (IS_DISP_OP_HFI(disp_op)) {
+		if (!args) {
+			SDE_ERROR("invalid args sent in opr event cb\n");
+			return;
 		}
 
-		atomic_inc(&sde_crtc->previous_opr_value.num_valid_opr);
-		if (current_opr_value[i] == sde_crtc->previous_opr_value.opr_value[i])
-			continue;
+		opr_data = (u32 *) args;
 
-		sde_crtc->previous_opr_value.opr_value[i] = current_opr_value[i];
-		updated = true;
+		if (sde_crtc->num_mixers != opr_data[0]) {
+			SDE_ERROR("num of valid opr doesn't match with number of mixers in crtc\n");
+			return;
+		}
+
+		atomic_set(&sde_crtc->previous_opr_value.num_valid_opr, opr_data[0]);
+		for (i = 0; i < sde_crtc->num_mixers; i++) {
+			if (opr_data[i+1] == sde_crtc->previous_opr_value.opr_value[i])
+				continue;
+
+			sde_crtc->previous_opr_value.opr_value[i] = opr_data[i+1];
+			updated = true;
+		}
+
+	} else {
+		for (i = 0; i < sde_crtc->num_mixers; i++) {
+			rc = sde_dspp_spr_read_opr_value(sde_crtc->mixers[i].hw_dspp,
+				&current_opr_value[i]);
+			if (rc) {
+				SDE_ERROR("failed to collect OPR idx: %d rc: %d\n", i, rc);
+				continue;
+			}
+
+			atomic_inc(&sde_crtc->previous_opr_value.num_valid_opr);
+			if (current_opr_value[i] == sde_crtc->previous_opr_value.opr_value[i])
+				continue;
+
+			sde_crtc->previous_opr_value.opr_value[i] = current_opr_value[i];
+			updated = true;
+		}
+
 	}
 
 	if (updated) {
@@ -3903,14 +3930,16 @@ static void _sde_crtc_frame_done_notify(struct drm_crtc *crtc,
 	struct sde_connector *sde_conn;
 	struct drm_encoder *encoder;
 	u32 frame_done = 1;
+	enum msm_disp_op disp_op = sde_crtc_get_disp_op(crtc);
 
 	sde_crtc = to_sde_crtc(crtc);
-	if (sde_crtc->opr_event_notify_enabled)
-		sde_crtc_opr_event_notify(crtc);
+
+	if (IS_DISP_OP_HWIO(disp_op) && sde_crtc->opr_event_notify_enabled)
+		sde_crtc_opr_event_notify(crtc, NULL);
 
 	sde_conn = to_sde_connector(fevent->connector);
-	if (sde_conn && sde_conn->misr_event_notify_enabled)
-		sde_encoder_misr_sign_event_notify(fevent->connector->encoder);
+	if (IS_DISP_OP_HWIO(disp_op) && sde_conn && sde_conn->misr_event_notify_enabled)
+		sde_encoder_misr_sign_event_notify(fevent->connector->encoder, NULL);
 
 	if (sde_crtc->framedone_event_notify_enabled)
 		sde_crtc_event_notify(crtc, DRM_EVENT_FRAME_DONE, &frame_done, sizeof(u32));
@@ -6471,6 +6500,7 @@ static bool skip_event_handling_required(struct drm_crtc *crtc, u32 event)
 	case DRM_EVENT_RGB_HIST_WB_ERR:
 	case DRM_EVENT_RGB_HIST_OFF:
 	case DRM_EVENT_HISTOGRAM:
+	case DRM_EVENT_OPR_VALUE:
 		return true;
 	default:
 		return false;
@@ -6868,6 +6898,10 @@ void sde_crtc_event_cb(void *data, u32 event, void *event_payload)
 		/* give event back */
 		sde_crtc_event_queue(&sde_crtc->base, sde_cp_notify_hist_event,
 				&sde_crtc->pa_hist_buffers[idx], true);
+		break;
+	case DRM_EVENT_OPR_VALUE:
+		sde_crtc_event_queue(&sde_crtc->base, sde_crtc_opr_event_notify,
+			event_payload, true);
 		break;
 	}
 }
@@ -10249,10 +10283,21 @@ static int sde_crtc_opr_event_handler(struct drm_crtc *crtc_drm,
 	bool en, struct sde_irq_callback *irq)
 {
 	struct sde_crtc *sde_crtc;
+	int ret = 0;
 
 	sde_crtc = to_sde_crtc(crtc_drm);
 	if (!sde_crtc)
 		return -EINVAL;
+
+	if (sde_crtc->hal_ops.enable_hw_event[MSM_DISP_OP_HFI]) {
+		ret = sde_crtc->hal_ops.enable_hw_event[MSM_DISP_OP_HFI](sde_crtc,
+			HFI_EVENT_SPR_OPR, en);
+		if (ret) {
+			DRM_ERROR("failed to enable SPR OPR event\n");
+			return ret;
+		}
+
+	}
 
 	sde_crtc->opr_event_notify_enabled = en;
 	return 0;
