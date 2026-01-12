d4d0d58a5561b50437baa0a08d5b668a9331824c
Rehan Kumar <rehakuma@qti.qualcomm.com>
2026-01-12 17:00:35 +0530

    disp: msm: sde: Add revision check for pebble target
    This change add revision check for pebble target.
    
    Change-Id: I43d560d73b75888eb5b6cf6a111610ac355bb77d
    Signed-off-by: Rehan Kumar <rehakuma@qti.qualcomm.com>

--- msm/sde/sde_hw_catalog.h
+++ msm/sde/sde_hw_catalog.h
@@ -73,6 +73,7 @@
 #define SDE_HW_VER_D00	SDE_HW_VER(13, 0, 0) /* canoe */
 #define SDE_HW_VER_D10	SDE_HW_VER(13, 1, 0) /* alor */
 #define SDE_HW_VER_E00  SDE_HW_VER(14, 0, 0) /* art */
+#define SDE_HW_VER_E30  SDE_HW_VER(14, 3, 0) /* pebble */
 
 #define SDE_QULTIVATE_SW_REV1 0x1
 
@@ -120,6 +121,7 @@
 #define IS_CANOE_TARGET(rev) IS_SDE_MAJOR_MINOR_SAME((rev), SDE_HW_VER_D00)
 #define IS_ALOR_TARGET(rev) IS_SDE_MAJOR_MINOR_SAME((rev), SDE_HW_VER_D10)
 #define IS_ART_TARGET(rev) IS_SDE_MAJOR_MINOR_SAME((rev), SDE_HW_VER_E00)
+#define IS_PEBBLE_TARGET(rev) IS_SDE_MAJOR_MINOR_SAME((rev), SDE_HW_VER_E30)
 
 #define SDE_HW_BLK_NAME_LEN	16
 
