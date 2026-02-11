512ece64062e72fce1107d2f0c3dd3523a0386b4
Spurthy Mutturaj <smuttura@qti.qualcomm.com>
2026-02-10 16:31:37 -0800

    Revert "msm: sde: hfi: disable power event for seraph target"
    This reverts commit ab6cca6ad82f3c09fa11ca54fe55697747e6aaa9.
    
    Change-Id: Ifbb098e62afae22f293ab54cfdec21a6ed19224d
    Signed-off-by: Spurthy Mutturaj <smuttura@qti.qualcomm.com>

--- msm/sde/hfi/hfi_crtc.c
+++ msm/sde/hfi/hfi_crtc.c
@@ -1007,7 +1007,6 @@ static int hfi_crtc_enable_hw_event(struct sde_crtc *crtc, u32 event, bool enabl
 	return ret;
 }
 
-#if !(IS_ENABLED(CONFIG_LSR_SERAPH))
 int hfi_crtc_set_idle_pc_timer(struct sde_crtc *sde_crtc, u32 val)
 {
 	struct hfi_cmdbuf_t *cmd_buf;
@@ -1062,12 +1061,6 @@ int hfi_crtc_set_idle_pc_timer(struct sde_crtc *sde_crtc, u32 val)
 
 	return rc;
 }
-#else
-int hfi_crtc_set_idle_pc_timer(struct sde_crtc *sde_crtc, u32 val)
-{
-	return 0;
-}
-#endif
 
 int _sde_crtc_hal_funcs_install(struct sde_crtc *crtc)
 {
