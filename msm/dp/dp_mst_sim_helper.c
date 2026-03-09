microsoft/surface-duo-oss-platform.vendor.opensource.display-drivers

disp: msm: dp: allow DP_MSTM_CTRL write in mst_skip mode
Allow DP_MSTM_CTRL write in mst_skip mode.

Change-Id: I173d0865f878642e46b20fd2b27e1740b0a9ea23
Signed-off-by: Xiaowen Wu <wxiaowen@codeaurora.org>

msm/dp/dp_mst_sim_helper.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019-2020, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2019-2021, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -951,7 +951,7 @@ int msm_dp_mst_sim_transfer(void *mst_sim_context, struct drm_dp_aux_msg *msg)
 			return msm_dp_mst_sim_clear_esi(mst_sim_context, msg);
 
 		if (msg->address == DP_MSTM_CTRL)
-			return msm_dp_mst_sim_reset(mst_sim_context, msg);
+			msm_dp_mst_sim_reset(mst_sim_context, msg);
 	} else if (msg->request == DP_AUX_NATIVE_READ) {
 		if (msg->address >= DP_SIDEBAND_MSG_DOWN_REP_BASE &&
 		    msg->address < DP_SIDEBAND_MSG_DOWN_REP_BASE + 256)

CodeLinaro-mirror/femto5g_platform_vendor_opensource_display-drivers

Signed-off-by: Jim Wang <quic_jimwan@quicinc.com>
(cherry picked from commit 38f4e6d8f5771ec2d237b50cf079c76be91daf73)

msm/dp/dp_mst_sim_helper.c
@@ -959,7 +959,7 @@ int dp_mst_sim_transfer(void *mst_sim_context, struct drm_dp_aux_msg *msg)
 			return dp_mst_sim_clear_esi(mst_sim_context, msg);
 
 		if (msg->address == DP_MSTM_CTRL)
-			return dp_mst_sim_reset(mst_sim_context, msg);
+			dp_mst_sim_reset(mst_sim_context, msg);
 
 	} else if (msg->request == DP_AUX_NATIVE_READ) {
 		if (msg->address >= DP_SIDEBAND_MSG_DOWN_REP_BASE &&