632d8da9645b557b00863a0095d61ff7677e08fc
Debanjalee Roy <debanjal@qti.qualcomm.com>
2026-03-16 21:11:00 -0700

    msm: disp: hfi: add hfi check to avoid process command for VHM
    This change avoids sending dcs command for VHM in HFI path.
    
    Change-Id: I58da07f757911125b1eeb45b959330ca8f1a0df0
    Signed-off-by: Debanjalee Roy <debanjal@qti.qualcomm.com>

--- msm/sde/sde_connector.c
+++ msm/sde/sde_connector.c
@@ -1847,7 +1847,7 @@ int sde_connector_update_cmd(struct drm_connector *connector,
 	struct sde_connector_state *c_state;
 	struct msm_display_conn_params params;
 	struct drm_encoder *drm_enc;
-	int rc;
+	int rc = 0;
 
 	if (!connector) {
 		SDE_ERROR("invalid argument\n");
@@ -1876,7 +1876,8 @@ int sde_connector_update_cmd(struct drm_connector *connector,
 	params.privacy_v1 = &c_state->privacy_v1;
 	params.b_lvl = c_conn->bl_dirty_value;
 
-	rc = c_conn->ops.process_dcs_cmd_bitmask(c_conn->display, &params);
+	if (sde_connector_get_disp_op(connector) == MSM_DISP_OP_HWIO)
+		rc = c_conn->ops.process_dcs_cmd_bitmask(c_conn->display, &params);
 
 	c_conn->last_vhm_cmd = cmd_bit_mask;
 	SDE_EVT32(connector->base.id, params.cmd_bit_mask >> 32,
