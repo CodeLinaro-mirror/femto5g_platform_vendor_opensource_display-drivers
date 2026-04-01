789b6b116a9ab8625955803bf7ae9f78a0c8ef5d
Garvis Garg <garvgarg@qti.qualcomm.com>
2026-04-01 11:04:49 +0530

    disp: msm: sde: add support for ssip fuse for pebble target
    This change adds support for ssip fuse check for pebble target.
    
    Change-Id: I629ed731a4a724ed430960f9ccbc202ab645f9e8
    Signed-off-by: Garvis Garg <garvgarg@qti.qualcomm.com>

--- msm/sde/sde_hw_catalog.c
+++ msm/sde/sde_hw_catalog.c
@@ -7537,7 +7537,8 @@ static int sde_hw_check_ssip_fuse(struct drm_device *dev, struct sde_mdss_cfg *s
 		disable = (fuse & BIT(1)) >> 1;
 		polarity = fuse & BIT(0);
 	} else if (IS_CANOE_TARGET(sde_cfg->hw_rev) ||
-			IS_ART_TARGET(sde_cfg->hw_rev)) {
+			IS_ART_TARGET(sde_cfg->hw_rev) ||
+			IS_PEBBLE_TARGET(sde_cfg->hw_rev)) {
 		disable = (fuse & BIT(5)) >> 5;
 		polarity = (fuse & BIT(3)) >> 3;
 	} else {
