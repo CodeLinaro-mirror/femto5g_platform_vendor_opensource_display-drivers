CodeLinaro-mirror/femto5g_platform_vendor_opensource_display-drivers

commit 4735dcceec9f3a5e9578f85675b771503ce1de72

msm: mixer resolution fix for ai scaler
This change support layer mixer update for ai scaler

Change-Id: I912e5ce334e32ba3628162b4de36af8bb854e356
Signed-off-by: mohinegi <mohinegi@qti.qualcomm.com>
Signed-off-by: Tanmay Karnavat <tkarnava@qti.qualcomm.com>

msm/sde/hfi/hfi_crtc.c
@@ -111,31 +111,46 @@ static bool _hfi_crtc_is_prop_excluded_for_repro(u32 drm_prop, enum wb_opmode op
 }
 #endif
 
-static int hfi_crtc_setup_resource_cfg(struct sde_crtc_state *cstate,
+static inline int get_num_mixers(struct sde_crtc_state *cstate,
+			struct sde_crtc *sde_crtc)
+{
+	if (!sde_crtc->num_mixers)
+		return 0;
+
+	return cstate->is_loopback_mode ? cstate->num_prim_mixers :
+			sde_crtc->num_mixers;
+}
+
+static int hfi_crtc_setup_resource_cfg(struct sde_crtc_state *cstate, struct sde_crtc *sde_crtc,
 		struct hfi_util_u32_prop_helper *prop_collector, u32 hfi_prop)
 {
 	struct hfi_resource_cfg lm_cfg;
 	int rc = 0;
+	int num_mixers;
 
-	/*
-	 * DS Config (Other types of configs can also be added below with respective checks and
-	 * their corresponding res_type)
-	 */
-	if (cstate->num_ds > 0) {
+	num_mixers = get_num_mixers(cstate, sde_crtc);
+
+	if (sde_crtc->ai_scaler_res.enabled && num_mixers) {
+		lm_cfg.res_type = HFI_RESOURCE_LM;
+		lm_cfg.resource_idx = 0;
+		lm_cfg.width = sde_crtc->ai_scaler_res.src_w / num_mixers;
+		lm_cfg.height = sde_crtc->ai_scaler_res.src_h;
+	} else if (cstate->num_ds_enabled) {
 		lm_cfg.res_type = HFI_RESOURCE_LM;
 		lm_cfg.resource_idx = cstate->ds_cfg[0].idx;
 		lm_cfg.width = cstate->ds_cfg[0].lm_width;
 		lm_cfg.height = cstate->ds_cfg[0].lm_height;
-
-		if (lm_cfg.width && lm_cfg.height) {
-			rc = hfi_util_u32_prop_helper_add_prop(prop_collector, hfi_prop,
-				HFI_VAL_U32_ARRAY, &lm_cfg, sizeof(struct hfi_resource_cfg));
-
-			return rc;
-		}
+	} else {
+		lm_cfg.res_type = HFI_RESOURCE_LM;
+		lm_cfg.resource_idx = 0;
+		lm_cfg.width =  0;
+		lm_cfg.height = 0;
 	}
 
-	return 0;
+	rc = hfi_util_u32_prop_helper_add_prop(prop_collector, hfi_prop, HFI_VAL_U32_ARRAY,
+		&lm_cfg, sizeof(struct hfi_resource_cfg));
+
+	return rc;
 }
 
 int _hfi_crtc_add_base_prop_helper(u32 hfi_prop, struct sde_crtc *crtc,
@@ -218,7 +233,7 @@ int _hfi_crtc_add_base_prop_helper(u32 hfi_prop, struct sde_crtc *crtc,
 		kfree(dim_layers);
 		break;
 	case HFI_PROPERTY_DISPLAY_SET_RESOURCE_DATA:
-		hfi_crtc_setup_resource_cfg(cstate, prop_collector, hfi_prop);
+		hfi_crtc_setup_resource_cfg(cstate, crtc, prop_collector, hfi_prop);
 		break;
 	default:
 		HFI_ERROR_CRTC(crtc_hfi, "unsupported HFI property\n");

commit a8a19352ed870a1c00ac5bf063736404a7e5e167

disp: msm: hfi: add check for property helper API
drm modetest is fails due to hfi_util_u32_prop_helper_add_prop() API
while width and height of lm_cfg is set to 0.

add check for lm_cfg dimension properties to avoid calling helper.

Fixes: 4735dcceec9f ("msm: mixer resolution fix for ai scaler")
Change-Id: Icd554891e637b7c604950a769f880fdbf7374395
Signed-off-by: Sanjay Chitroda <ckantibh@qti.qualcomm.com>

msm/sde/hfi/hfi_crtc.c
@@ -155,8 +155,10 @@ static int hfi_crtc_setup_resource_cfg(struct sde_crtc_state *cstate, struct sde
 		lm_cfg.height = 0;
 	}
 
-	rc = hfi_util_u32_prop_helper_add_prop(prop_collector, hfi_prop, HFI_VAL_U32_ARRAY,
-		&lm_cfg, sizeof(struct hfi_resource_cfg));
+	if (lm_cfg.width && lm_cfg.height) {
+		rc = hfi_util_u32_prop_helper_add_prop(prop_collector, hfi_prop,
+			HFI_VAL_U32_ARRAY, &lm_cfg, sizeof(struct hfi_resource_cfg));
+	}
 
 	return rc;
 }